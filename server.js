50c493fd5d4eb4aa9d564e147ffba4779d7333f8
Alex Young <alex@alexyoung.info>
2016-12-22 22:17:30 +0000

    change ip parsing

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var app = require('express')();
 app.use(require('express-useragent').express());
 
 app.get('/', (req, res) => {
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    if (ip.substr(0, 7) == "::ffff:") {
+      ip = ip.substr(7)
+    }
     res.send({
-        ipaddress: req.ip,
+        ipaddress: ip,
         languages: req.acceptsLanguages(),
         software: req.useragent.os
     });
